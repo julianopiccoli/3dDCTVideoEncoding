56b36d9c93b169ef400a81bb9fa4c4e99e8ee707
Piccoli <Piccoli@LAPTOP-UHK25PO0>
2020-08-28 22:20:41 -0300

    Changes build system from internal CDT mechanism to Gnu Make

---
+++ 3d-DCT-video-encoding-OpenCL/Default/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+..\CubeUtils.c \
+..\ExpGolomb.c \
+..\OpenCLUtils.c \
+..\decoder.c \
+..\encoder.c \
+..\main.c 
+
+OBJS += \
+.\CubeUtils.o \
+.\ExpGolomb.o \
+.\OpenCLUtils.o \
+.\decoder.o \
+.\encoder.o \
+.\main.o 
+
+C_DEPS += \
+.\CubeUtils.d \
+.\ExpGolomb.d \
+.\OpenCLUtils.d \
+.\decoder.d \
+.\encoder.d \
+.\main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: C Compiler'
+	gcc -I"..\..\..\OpenCL-Headers" -O0 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
